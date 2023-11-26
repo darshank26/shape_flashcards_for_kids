12539e9561371b1adf7ed30189616b539caecd53
darshan <darsh2605@gmail.com>
2023-11-26 22:09:46 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/shape_flashcards_for_kids/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darshankomu.shape_flashcards_for_kids
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+const kprimarycolor = Color(0xff1C3249);
+const ksecondcolor = Color(0xff3ba2ff);
+const kthirdcolor = Color(0xff006ac0);
+const ksplashback = Color(0xffFF8D9B);
+const kback = Color(0xffFF8D9B);
+const backcolor = Color(0xFFFCFFFC);
+
+
+
+var androidAppShareLink = "https://play.google.com/store/apps/details?id=com.darshankomu.shape_flashcards_for_kids";
+
+var androidShareText = "Download Shape Flashcards for Kids app from below link";
+
+var androidAppIdValue = "com.darshankomu.shape_flashcards_for_kids";
+
+var iOSAppIdValue = "AddiOSAppId";
